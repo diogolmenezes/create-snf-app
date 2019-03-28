3e18a5276877d98c6f7e100a9be25905bfe8de7d
Diogo Menezes <diogolmenezes@gmail.com>
2019-03-28 10:00:41 -0300

    remove bug space

--- createSnfApp.js
+++ createSnfApp.js
@@ -160,12 +160,12 @@ function replaceParameters(path, name, port) {
 
 function success(path, name) {
     const configPath = `${name}/api/config/env/default.js`;
-    const install = (program.disableInstall) ? 'npm install &&' : '';
+    const install = (program.disableInstall) ? ' npm install &&' : '';
 
     console.log(`${chalk.green(`SUCCESS! Application installed at ${chalk.blue(path)}`)}`);
     console.log();
     console.log(`  1. Change configurations at ${chalk.blue(configPath)}.`);
-    console.log(`  2. Start your app ${chalk.blue(`cd ${name} && ${install} npm start`)}`);
+    console.log(`  2. Start your app ${chalk.blue(`cd ${name} &&${install} npm start`)}`);
     console.log();
     console.log('Documentation:');
     console.log();
